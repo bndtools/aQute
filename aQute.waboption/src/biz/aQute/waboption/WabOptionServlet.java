898b9fd65d7b6f12b81d6419504c459bd650bd2b
Peter Kriens <peter.kriens@aqute.biz>
2011-05-19 16:46:17 +0200

    Split project in aQute.war and aQute.gwt

--- aQute.waboption/src/biz/aQute/waboption/WabOptionServlet.java
+++ aQute.waboption/src/biz/aQute/waboption/WabOptionServlet.java
@@ -1,5 +1,15 @@
 package biz.aQute.waboption;
 
-public class WabOptionServlet {
+import java.io.*;
 
+import javax.servlet.http.*;
+
+public class WabOptionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override 
+	public void doGet(HttpServletRequest rq, HttpServletResponse rsp) throws IOException {
+		PrintWriter writer = rsp.getWriter();
+		writer.println("<html><body><img src='icon.gif'/>Hello World</body></html>");
+	}
 }

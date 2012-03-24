9f5ac93f89773ff19df9e0b870e3314d682010ce
Peter Kriens <peter.kriens@aqute.biz>
2012-03-24 09:02:25 +0100

    Added version and wrapper

--- aQute.timer/src/aQute/timer/Timer.java
+++ aQute.timer/src/aQute/timer/Timer.java
@@ -1,9 +1,77 @@
 package aQute.timer;
 
+import java.util.*;
+
 import aQute.bnd.annotation.component.*;
 
 
-@Component(provide=java.util.Timer.class)
+@Component(provide=java.util.Timer.class, servicefactory=true)
 public class Timer extends java.util.Timer {
+	final List<TimerTask> tasks = new ArrayList<TimerTask>();
+	
+	
+	@Deactivate
+	void deactivate() {
+		
+	}
+
+	
+	class Wrapper extends TimerTask {
+		TimerTask task;
+		
+		Wrapper(TimerTask t) {
+		}
+
+		@Override
+		public void run() {
+			try {
+				task.run();
+			} finally {
+				tasks.remove(this);
+			}
+		}
+		
+		@Override
+		public boolean cancel(){
+			tasks.remove(this);
+			return super.cancel();
+		}		
+	}
+	
+	
+	@Override
+	public void schedule(TimerTask t, long delay, long period) {
+		super.schedule(wrap(t), delay, period);
+	}
+	@Override
+	public void schedule(TimerTask t, Date date) {
+		super.schedule(wrap(t), date);
+	}
+	
+	@Override
+	public void schedule(TimerTask t, Date date, long period) {
+		super.schedule(wrap(t), date, period);
+	}
+
+	@Override
+	public void schedule(TimerTask t,long delay) {
+		super.schedule(wrap(t), delay);
+	}
+	
+	@Override
+	public void scheduleAtFixedRate(TimerTask t, Date date, long period) {
+		super.scheduleAtFixedRate(wrap(t), date, period);		
+	}
+
+	@Override
+	public void scheduleAtFixedRate(TimerTask t, long delay, long period) {
+		super.scheduleAtFixedRate(wrap(t), delay, period);
+	}
+
+	private TimerTask wrap(TimerTask t) {
+		Wrapper w = new Wrapper(t);
+		tasks.add(w);
+		return w;
+	}
 	
 }

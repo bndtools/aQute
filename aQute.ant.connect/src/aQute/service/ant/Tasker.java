c8ac43b63faa766976b4e7850c7ee05da049137c
Peter Kriens <peter.kriens@aqute.biz>
2012-05-11 13:43:31 +0200

    removed some unneeded shit

--- aQute.ant.connect/src/aQute/service/ant/Tasker.java
+++
@@ -1,8 +0,0 @@
-package aQute.service.ant;
-
-import org.apache.tools.ant.*;
-
-
-public interface Tasker<T extends Task> {
-	void execute(T task) throws BuildException;
-}
--- aQute.gwt/src/aQute/gwt/greeter/shared/Custom.java
+++
@@ -1,7 +0,0 @@
-package aQute.gwt.greeter.shared;
-
-import com.google.gwt.user.client.rpc.*;
-
-public class Custom implements IsSerializable {
-	String x = "x";
-}

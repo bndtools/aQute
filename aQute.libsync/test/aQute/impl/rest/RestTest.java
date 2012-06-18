6bc878df51ab6d427af0c55b2ce6ce720e8988cb
Peter Kriens <peter.kriens@aqute.biz>
2012-06-18 08:43:44 +0200

    Test cases for rest, need to be implemented

---
+++ aQute.libsync/test/aQute/impl/rest/RestTest.java
@@ -0,0 +1,64 @@
+package aQute.impl.rest;
+
+import java.io.*;
+import java.util.*;
+
+import junit.framework.*;
+import aQute.service.rest.*;
+
+public class RestTest extends TestCase {
+
+	public class Card {
+		String	_id;
+		String	nameOnCard;
+	}
+
+	public class User {
+		public byte[]		_id;
+		public String		name;
+		Collection<Card>	cards;
+
+	}
+
+	public class UserManaer implements ResourceManager {
+
+//		// PUT /rest/user
+//		public String putUser( User user) {return null;}
+//		
+//		// PUT /rest/user/123
+//		public void putUser( User user, String id) {}
+//		
+//		public void postCard( options options, String userId) {
+//			
+//		}
+//		public User get(options options, String id) {
+//
+//		}
+//
+//		public Card getCard(options options, String userId, String cardId) {
+//
+//		}
+//
+//		public Iterable<Card> getCard(options options, String userId) {
+//
+//		}
+//		
+//
+//		public Card getCard(options options) {
+//
+//		}
+//
+//		public Iterable<User> get(query query) {
+//
+//		}
+//
+//		public void delete(String id) {
+//
+//		}
+//
+//		public void deleteCard(String userId, String cardId) {
+//
+//		}
+//
+	}
+}
